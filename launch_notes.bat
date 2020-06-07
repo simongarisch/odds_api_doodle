e6d18cec4405d106cd7925b9cdc51930e0a5e4a4
simongarisch <gatman946@gmail.com>
2020-06-07 14:53:59 +1000

    adding batch file to launch docker notes

---
+++ launch_notes.bat
@@ -0,0 +1,4 @@
+REM docker pull gopherdata/gophernotes:latest-ds
+REM docker image ls
+REM docker run gopherdata/gophernotes:latest-ds
+docker run -it -p 8888:8888 -v C:\Users\simon.garisch\Desktop\git\odds_api_doodle:/bin/ gopherdata/gophernotes:latest-ds
